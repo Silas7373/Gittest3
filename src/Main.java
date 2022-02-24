1f78744934f583bac1846d14a91bce7b6fe8739b
Silas <silas.kocher@htl-saalfelden.at>
2022-02-24 11:55:57 +0100

    initial2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("LOL");
         System.out.println("Hi");
+        System.out.println("das ist funny");
     }
 }
